09c48b9ad4b30451fd73c17a24f20bc002f4a992
ololade <ololadelawal@gmail.com>
2022-12-20 17:57:52 -0800

    update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -80,4 +80,4 @@ class Square:
         elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-           self.__position = value
+            self.__position = value
