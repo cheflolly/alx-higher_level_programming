7139047ec076ec3441ad53beb48e6a6e446b2b04
ololade <ololadelawal@gmail.com>
2023-03-15 12:07:07 -0700

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,9 @@ if (isNaN(size)) {
 } else {
   for (let r = 0; r < size; r++) {
     let row = '';
-    for (let c = 0; c < size; c++) row += 'X';
+    for (let c = 0; c < size; c++) {
+      row += 'X';
+    }
     console.log(row);
   }
 }
